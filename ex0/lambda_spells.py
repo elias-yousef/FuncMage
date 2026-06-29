110ba0209156010245662e3ea0ff36d23d862be6
eabushak <eabushak@c2r6s5.irbid.42.school>
2026-06-29 16:37:15 +0300

    done

--- ex0/lambda_spells.py
+++ ex0/lambda_spells.py
@@ -1,29 +1,10 @@
-from typing import Any
-
-
-artifacts = [
-            {'name': 'Storm Crown', 'power': 64, 'type': 'accessory'},
-            {'name': 'Ice Wand', 'power': 96, 'type': 'focus'},
-            {'name': 'Ice Wand', 'power': 79, 'type': 'focus'},
-            {'name': 'Storm Crown', 'power': 80, 'type': 'accessory'}
-              ]
-mages = [
-        {'name': 'Sage', 'power': 96, 'element': 'lightning'},
-        {'name': 'Zara', 'power': 88, 'element': 'earth'},
-        {'name': 'Ember', 'power': 56, 'element': 'lightning'},
-        {'name': 'Kai', 'power': 58, 'element': 'ice'},
-        {'name': 'Nova', 'power': 60, 'element': 'shadow'}
-          ]
-spells = ['tornado', 'shield', 'heal', 'earthquake']
-
-
-def artifact_sorter(artifacts: list[dict[str, Any]]) -> list[dict[str, Any]]:
+def artifact_sorter(artifacts: list[dict[str]]) -> list[dict[str]]:
     return (list(sorted(artifacts, key=lambda x: x['power'], reverse=True)))
 
 
 def power_filter(
-        mages: list[dict[str, Any]], min_power: int
-        ) -> list[dict[str, Any]]:
+        mages: list[dict[str]], min_power: int
+        ) -> list[dict[str]]:
     return (list(filter(lambda x: x['power'] >= min_power, mages)))
 
 
@@ -31,7 +12,7 @@ def spell_transformer(spells: list[str]) -> list[str]:
     return (list(map(lambda x: "* " + x + " * ", spells)))
 
 
-def mage_stats(mages: list[dict[str, Any]]) -> dict[str, Any]:
+def mage_stats(mages: list[dict[str]]) -> dict[str]:
     minimum = min(mages, key=lambda x: x['power'])['power']
     maximum = max(mages, key=lambda x: x['power'])['power']
     avg = maximum - minimum / len(mages)
@@ -43,16 +24,30 @@ def mage_stats(mages: list[dict[str, Any]]) -> dict[str, Any]:
 
 
 if __name__ == "__main__":
+    artifacts = [
+            {'name': 'Storm Crown', 'power': 64, 'type': 'accessory'},
+            {'name': 'Ice Wand', 'power': 96, 'type': 'focus'},
+            {'name': 'Ice Wand', 'power': 79, 'type': 'focus'},
+            {'name': 'Storm Crown', 'power': 80, 'type': 'accessory'}
+              ]
+    mages = [
+        {'name': 'Sage', 'power': 96, 'element': 'lightning'},
+        {'name': 'Zara', 'power': 88, 'element': 'earth'},
+        {'name': 'Ember', 'power': 56, 'element': 'lightning'},
+        {'name': 'Kai', 'power': 58, 'element': 'ice'},
+        {'name': 'Nova', 'power': 60, 'element': 'shadow'}
+          ]
+    spells = ['tornado', 'shield', 'heal', 'earthquake']
     print("Testing artifact sorter...")
     sorted_list = artifact_sorter(artifacts)
     print(f"{sorted_list[0]['name']} \
 ({sorted_list[0]['power']} power) comes before \
 {sorted_list[1]['name']} ({sorted_list[1]['power']} power)")
     print("\nTesting power filter...")
-    print(power_filter(mages, 60), '\n')
+    print(power_filter(mages, 60))
     print("\nTesting spell transformer...")
     list_spells = spell_transformer(spells)
     str_spells = "".join(list_spells)
-    print(str_spells, '\n')
+    print(str_spells)
     print("\nTesting mage stats")
     print(mage_stats(mages))
--- ex1/higher_magic.py
+++ ex1/higher_magic.py
@@ -1,15 +1,68 @@
 from collections.abc import Callable
 
 
+def heal(target: str, power: int) -> str:
+    return f"Heal {target}"
+
+
+def fire(target: str, power: int) -> str:
+    return f"Fireball hits {target}"
+
+
+def Condition(target: str, power: int) -> bool:
+    if power > 20:
+        return True
+    else:
+        return False
+
+
 def spell_combiner(spell1: Callable, spell2: Callable) -> Callable:
-    def new_func(spell1: Callable, spell2: Callable) -> Callable:
-        spell1()
-        spell2()
-    return (new_func)
+    def spell(target: str, power: int) -> tuple:
+        first = spell1(target, power)
+        second = spell2(target, power)
+        return (first, second)
+    return (spell)
+
 
 def power_amplifier(base_spell: Callable, multiplier: int) -> Callable:
-    pass
+    def amplifire(target: str, power: int) -> Callable:
+        Amplified = power * multiplier
+        print(f"Original: {power}, Amplified: {Amplified}")
+        return (base_spell(target, Amplified))
+    return (amplifire)
+
+
 def conditional_caster(condition: Callable, spell: Callable) -> Callable:
-    pass
+    def guarded_spell(target: str, power: int) -> str:
+        if condition(target, power):
+            return spell(target, power)
+        else:
+            return "Spell fizzled"
+    return (guarded_spell)
+
+
 def spell_sequence(spells: list[Callable]) -> Callable:
-    pass
+    def cast_list(target: str, power: int) -> list:
+        spell_list = []
+        for spell in spells:
+            spell_list.append(spell(target, power))
+        return (spell_list)
+    return (cast_list)
+
+
+if __name__ == "__main__":
+    mega_spell = spell_combiner(fire, heal)
+    final_out = mega_spell("Dragon", 20)
+    print("Testing spell combiner...")
+    print(f"Combined spell result: {" ".join(final_out)}")
+    print("Testing power amplifier...")
+    amp_spell = power_amplifier(heal, 5)
+    amp_massege = amp_spell("Dragon", 15)
+    print("Testing conditional caster...")
+    example3 = conditional_caster(Condition, heal)
+    new_output = example3("Dragon", 25)
+    print(new_output)
+    print("Testing spell sequence...")
+    sequence = spell_sequence([heal, fire])
+    final_out = sequence("Dragon", 25)
+    print(" ".join(final_out))
